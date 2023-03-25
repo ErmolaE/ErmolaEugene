5687ff0c3cff551df318729f53ecba2634abe216
ErmolaE <86079025+ErmolaE@users.noreply.github.com>
2023-03-25 18:53:40 +0300

    add_lists_details

--- Bootcamp_Test_Task/Bootcamp_Test_Task/urls.py
+++ Bootcamp_Test_Task/Bootcamp_Test_Task/urls.py
@@ -22,11 +22,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('books_service/', include('books_service.urls')),
+    path('', RedirectView.as_view(url='/books_service/', permanent=True)),
 ]
 
-urlpatterns += [
-     path('books_service/', include('books_service.urls')),
-     path('', RedirectView.as_view(url='/books_service/', permanent=True)),
-]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Bootcamp_Test_Task/books_service/views.py
+++ Bootcamp_Test_Task/books_service/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Book, Author, Language, Genre
 
 # Create your views here.
+
+
+def index(request):
+    """
+    Display function for the home page of the site.
+    """
+    # Generation of "quantities" of main objects
+    num_books=Book.objects.all().count()
+    num_authors=Author.objects.all().count() 
+
+    # Rendering the HTML template index.html with data inside variable context
+    return render(
+        request,
+        'index.html',
+        context={'num_books':num_books,'num_authors':num_authors},
+    )
+
+
+class BookListView(generic.ListView):
+    """Generic class-based view for a list of books."""
+    model = Book
+    paginate_by = 20
+
+
+class BookDetailView(generic.DetailView):
+    """Generic class-based detail view for a book."""
+    model = Book
+
+
+class AuthorListView(generic.ListView):
+    """Generic class-based list view for a list of authors."""
+    model = Author
+    paginate_by = 20
+
+
+class AuthorDetailView(generic.DetailView):
+    """Generic class-based detail view for an author."""
+    model = Author
