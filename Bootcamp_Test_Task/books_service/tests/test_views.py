5c92c413e01eb338f3c564ddd6a685d1ee4fb6ae
ErmolaE <86079025+ErmolaE@users.noreply.github.com>
2023-03-26 17:39:49 +0300

    add_test

---
+++ Bootcamp_Test_Task/books_service/tests/test_views.py
@@ -0,0 +1,76 @@
+from django.test import TestCase
+from books_service.models import Author, Book, Genre, Language
+from django.urls import reverse
+from django.contrib.auth.models import User, Permission
+import datetime
+
+
+
+class AuthorListViewTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        # Create authors for pagination tests
+        number_of_authors = 23
+        for author_id in range(number_of_authors):
+            Author.objects.create(first_name='Christian {0}'.format(author_id),
+                                  last_name='Surname {0}'.format(author_id))
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/books_service/authors/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('authors'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('authors'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'books_service/author_list.html')
+
+    def test_pagination_is_ten(self):
+        response = self.client.get(reverse('authors'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('is_paginated' in response.context)
+        self.assertTrue(response.context['is_paginated'] is True)
+        self.assertEqual(len(response.context['author_list']), 20)
+
+    def test_lists_all_authors(self):
+        # Get second page and confirm it has (exactly) the remaining 3 items
+        response = self.client.get(reverse('authors')+'?page=2')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('is_paginated' in response.context)
+        self.assertTrue(response.context['is_paginated'] is True)
+        self.assertEqual(len(response.context['author_list']), 3)
+
+
+class AuthorCreateViewTest(TestCase):
+    """Test case for the AuthorCreate view."""
+
+    def setUp(self):
+        # Create a user
+        test_user1 = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
+        test_user2 = User.objects.create_user(username='testuser2', password='2HJ1vRV0Z&3iD')
+
+        test_user1.save()
+        test_user2.save()
+
+    def test_logged_in_with_permission(self):
+        login = self.client.login(username='testuser2', password='2HJ1vRV0Z&3iD')
+        response = self.client.get(reverse('author-create'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_uses_correct_template(self):
+        login = self.client.login(username='testuser2', password='2HJ1vRV0Z&3iD')
+        response = self.client.get(reverse('author-create'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'books_service/author_form.html')
+
+    def test_redirects_to_detail_view_on_success(self):
+        login = self.client.login(username='testuser2', password='2HJ1vRV0Z&3iD')
+        response = self.client.post(reverse('author-create'),
+                                    {'first_name': 'Christian Name', 'last_name': 'Surname'})
+        # Manually check redirect because we don't know what author was created
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(response.url.startswith('/books_service/author/'))
